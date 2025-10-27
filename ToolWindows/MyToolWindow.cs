2d32689098183e2d3f2182a561fab761ec3135eb
Arman Sahakyan <arman_sahakyan@hotmail.com>
2025-10-28 00:07:25 +0400

    Initial commit

---
+++ ToolWindows/MyToolWindow.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.Imaging;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ChatAssistantVSIX
+{
+  public class MyToolWindow : BaseToolWindow<MyToolWindow>
+  {
+    public override string GetTitle(int toolWindowId) => "My Tool Window";
+
+    public override Type PaneType => typeof(Pane);
+
+    public override Task<FrameworkElement> CreateAsync(int toolWindowId, CancellationToken cancellationToken)
+    {
+      return Task.FromResult<FrameworkElement>(new MyToolWindowControl());
+    }
+
+    [Guid("822ff34f-53b6-47c4-a1c6-c84b9464f79c")]
+    internal class Pane : ToolkitToolWindowPane
+    {
+      public Pane()
+      {
+        BitmapImageMoniker = KnownMonikers.ToolWindow;
+      }
+    }
+  }
+}
